e04efce4bfe531f49fadc8eb5643a09933742b58
timo <22354443+tnkuehne@users.noreply.github.com>
2025-05-29 18:01:24 +0200

    first example cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use clap::Parser;
+
+/// A simple CLI tool example
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name to greet
+    #[arg(short, long, default_value = "World")]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u32,
+
+    /// Make the greeting uppercase
+    #[arg(short, long)]
+    uppercase: bool,
+
+    /// Enable verbose output
+    #[arg(short, long)]
+    verbose: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if args.verbose {
+        println!("Greeting '{}' {} time(s)", args.name, args.count);
+        if args.uppercase {
+            println!("Using uppercase formatting");
+        }
+    }
+
+    for i in 1..=args.count {
+        let greeting = format!("Hello, {}!", args.name);
+        let output = if args.uppercase {
+            greeting.to_uppercase()
+        } else {
+            greeting
+        };
+        
+        if args.verbose {
+            println!("[{}] {}", i, output);
+        } else {
+            println!("{}", output);
+        }
+    }
 }
